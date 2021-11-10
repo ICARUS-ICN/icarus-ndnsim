9cc32958d0198147303ad008027a48285180a6c9
Miguel Rodríguez Pérez <miguel@det.uvigo.gal>
2021-11-10 11:52:01 +0100

    Add a new transport for Ground to satellite devices

---
+++ model/ground-sta-transport.cc
@@ -0,0 +1,165 @@
+/* -*- Mode:C++; c-file-style:"gnu"; indent-tabs-mode:nil; -*- */
+/*
+ *
+ * Copyright (c) 2021 Universidade de Vigo
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation;
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
+ * Author: Miguel Rodríguez Pérez <miguel@det.uvigo.gal>
+ *
+ */
+
+#include "ground-sta-transport.h"
+#include "model/ndn-block-header.hpp"
+#include "model/ndn-l3-protocol.hpp"
+#include "ns3/assert.h"
+#include "ns3/constellation.h"
+#include "ns3/log-macros-enabled.h"
+#include "ns3/mobility-model.h"
+#include "ns3/node.h"
+#include "ns3/pointer.h"
+#include "ns3/queue.h"
+#include "ns3/sat2ground-net-device.h"
+
+NS_LOG_COMPONENT_DEFINE ("icarus.ndn.GroundStaTransport");
+
+namespace ns3 {
+namespace ndn {
+namespace icarus {
+
+GroundStaTransport::GroundStaTransport (Ptr<Node> node, const Ptr<NetDevice> &netDevice,
+                                        const std::string &localUri, const std::string &remoteUri,
+                                        ::ndn::nfd::FaceScope scope,
+                                        ::ndn::nfd::FacePersistency persistency,
+                                        ::ndn::nfd::LinkType linkType)
+    : m_netDevice (DynamicCast<::ns3::icarus::GroundStaNetDevice> (netDevice)), m_node (node)
+{
+  NS_LOG_FUNCTION (this << node << netDevice << localUri << remoteUri);
+
+  this->setLocalUri (FaceUri (localUri));
+  this->setRemoteUri (FaceUri (remoteUri));
+  this->setScope (scope);
+  this->setPersistency (persistency);
+  this->setLinkType (linkType);
+  this->setMtu (m_netDevice->GetMtu ()); // Use the MTU of the netDevice
+
+  // Get send queue capacity for congestion marking
+  PointerValue txQueueAttribute;
+  if (m_netDevice->GetAttributeFailSafe ("TxQueue", txQueueAttribute))
+    {
+      Ptr<ns3::QueueBase> txQueue = txQueueAttribute.Get<ns3::QueueBase> ();
+      // must be put into bytes mode queue
+
+      auto size = txQueue->GetMaxSize ();
+      if (size.GetUnit () == BYTES)
+        {
+          this->setSendQueueCapacity (size.GetValue ());
+        }
+      else
+        {
+          // don't know the exact size in bytes, guessing based on "standard" packet size
+          this->setSendQueueCapacity (size.GetValue () * 1500);
+        }
+    }
+
+  NS_LOG_FUNCTION (this << "Creating an ndnSIM transport instance for netDevice with URI"
+                        << this->getLocalUri ());
+
+  NS_ASSERT_MSG (m_netDevice != 0, "NetDeviceFace needs to be assigned a valid NetDevice");
+
+  m_node->RegisterProtocolHandler (MakeCallback (&GroundStaTransport::receiveFromNetDevice, this),
+                                   L3Protocol::ETHERNET_FRAME_TYPE, m_netDevice,
+                                   true /*promiscuous mode*/);
+}
+
+GroundStaTransport::~GroundStaTransport ()
+{
+  NS_LOG_FUNCTION_NOARGS ();
+}
+
+ssize_t
+GroundStaTransport::getSendQueueLength ()
+{
+  NS_LOG_FUNCTION (this);
+
+  PointerValue txQueueAttribute;
+  if (m_netDevice->GetAttributeFailSafe ("TxQueue", txQueueAttribute))
+    {
+      Ptr<ns3::QueueBase> txQueue = txQueueAttribute.Get<ns3::QueueBase> ();
+      return txQueue->GetNBytes ();
+    }
+  else
+    {
+      return nfd::face::QUEUE_UNSUPPORTED;
+    }
+}
+
+void
+GroundStaTransport::doClose ()
+{
+  NS_LOG_FUNCTION (this << "Closing transport for netDevice with URI" << this->getLocalUri ());
+
+  // set the state of the transport to "CLOSED"
+  this->setState (nfd::face::TransportState::CLOSED);
+}
+
+void
+GroundStaTransport::doSend (const Block &packet, const nfd::EndpointId &endpoint)
+{
+  NS_LOG_FUNCTION (this << "Sending packet from netDevice with URI" << this->getLocalUri ());
+
+  // convert NFD packet to NS3 packet
+  BlockHeader header (packet);
+
+  Ptr<ns3::Packet> ns3Packet = Create<ns3::Packet> ();
+  ns3Packet->AddHeader (header);
+
+  const auto channel = DynamicCast<::ns3::icarus::GroundSatChannel> (m_netDevice->GetChannel ());
+  NS_ASSERT_MSG (channel != nullptr, "This net devices is not attached to a valid channel");
+
+  NS_LOG_WARN ("We may try different alternatives to find a valid satellite");
+  NS_LOG_WARN ("FIXME: Just a hack. We should be using the endpoint or some header for this.");
+  const auto mobility_model = m_node->GetObject<MobilityModel> ();
+  NS_ASSERT (mobility_model != nullptr);
+  const auto dst = channel->GetConstellation ()->GetClosest (mobility_model->GetPosition ());
+  // send the NS3 packet
+  m_netDevice->Send (ns3Packet, dst->GetAddress (), L3Protocol::ETHERNET_FRAME_TYPE);
+}
+
+// callback
+void
+GroundStaTransport::receiveFromNetDevice (Ptr<NetDevice> device, Ptr<const ns3::Packet> p,
+                                          uint16_t protocol, const Address &from, const Address &to,
+                                          NetDevice::PacketType packetType)
+{
+  NS_LOG_FUNCTION (device << p << protocol << from << to << packetType);
+
+  // Convert NS3 packet to NFD packet
+  Ptr<ns3::Packet> packet = p->Copy ();
+
+  BlockHeader header;
+  packet->RemoveHeader (header);
+
+  this->receive (std::move (header.getBlock ()));
+}
+
+Ptr<NetDevice>
+GroundStaTransport::GetNetDevice () const
+{
+  return m_netDevice;
+}
+
+} // namespace icarus
+} // namespace ndn
+} // namespace ns3
---
+++ model/ground-sta-transport.h
@@ -0,0 +1,70 @@
+/* -*- Mode:C++; c-file-style:"gnu"; indent-tabs-mode:nil; -*- */
+/*
+ *
+ * Copyright (c) 2021 Universidade de Vigo
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation;
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
+ * Author: Miguel Rodríguez Pérez <miguel@det.uvigo.gal>
+ *
+ */
+
+#ifndef GROUND_STA_TRANSPORT_H
+#define GROUND_STA_TRANSPORT_H
+
+#include "ns3/ground-sta-net-device.h"
+#include "ns3/net-device.h"
+#include "daemon/face/transport.hpp"
+#include "model/ndn-common.hpp"
+
+namespace ns3 {
+
+class Node;
+
+namespace ndn {
+namespace icarus {
+
+class GroundStaTransport : public nfd::face::Transport
+{
+public:
+  GroundStaTransport (
+      Ptr<Node> node, const Ptr<NetDevice> &netDevice, const std::string &localUri,
+      const std::string &remoteUri, ::ndn::nfd::FaceScope scope = ::ndn::nfd::FACE_SCOPE_NON_LOCAL,
+      ::ndn::nfd::FacePersistency persistency = ::ndn::nfd::FACE_PERSISTENCY_PERSISTENT,
+      ::ndn::nfd::LinkType linkType = ::ndn::nfd::LINK_TYPE_AD_HOC);
+
+  ~GroundStaTransport ();
+
+  Ptr<NetDevice> GetNetDevice () const;
+
+  virtual ssize_t getSendQueueLength () final;
+
+private:
+  virtual void doClose () override;
+
+  virtual void doSend (const Block &packet, const nfd::EndpointId &endpoint) override;
+
+  void receiveFromNetDevice (Ptr<NetDevice> device, Ptr<const ns3::Packet> p, uint16_t protocol,
+                             const Address &from, const Address &to,
+                             NetDevice::PacketType packetType);
+
+  Ptr<::ns3::icarus::GroundStaNetDevice> m_netDevice; ///< \brief Smart pointer to NetDevice
+  Ptr<Node> m_node;
+};
+
+} // namespace icarus
+} // namespace ndn
+} // namespace ns3
+
+#endif
