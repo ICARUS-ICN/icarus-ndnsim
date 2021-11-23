5191af4657e144888bf5efc306b0a470908e8196
sherreria <sherreria@gmail.com>
2021-11-23 11:08:44 +0100

    Added new MAC Aloha model for the ground2sat channel

---
+++ model/aloha-mac-model.cc
@@ -0,0 +1,107 @@
+/* -*- Mode:C++; c-file-style:"gnu"; indent-tabs-mode:nil; -*- */
+/*
+ * Copyright (c) 2021 Universidade de Vigo
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * Author: Sergio Herrería Alonso <sha@det.uvigo.es>
+ */
+
+#include "aloha-mac-model.h"
+#include "ns3/log-macros-enabled.h"
+#include "ns3/log.h"
+#include "ns3/simulator.h"
+
+namespace ns3 {
+namespace icarus {
+
+  NS_LOG_COMPONENT_DEFINE ("icarus.AlohaMacModel");
+
+  NS_OBJECT_ENSURE_REGISTERED (AlohaMacModel);
+    
+  TypeId
+  AlohaMacModel::GetTypeId (void)
+  {
+    static TypeId tid =
+      TypeId ("ns3::icarus::AlohaMacModel")
+      .SetParent<MacModel> ()
+      .SetGroupName ("ICARUS")
+      .AddConstructor<AlohaMacModel> ();
+    
+    return tid;
+  }
+
+  AlohaMacModel::AlohaMacModel ()
+  {
+    NS_LOG_FUNCTION (this);
+    
+    busy_period_packet_uid = 0;
+    busy_period_collision = false;
+  }
+
+  MacModel::MacProtocol
+  AlohaMacModel::GetMacProtocol (void) const
+  {
+    NS_LOG_FUNCTION (this);
+    
+    return ALOHA;
+  }
+    
+  void
+  AlohaMacModel::NewPacketRx (uint64_t packet_uid, Time packet_tx_time)
+  {
+    NS_LOG_FUNCTION (this << packet_uid << packet_tx_time);
+    
+    if (busy_period_packet_uid > 0)
+      {
+        busy_period_collision = true;
+        NS_LOG_LOGIC ("Packet " << packet_uid << " causes collision");
+      }
+    
+    Time finish_tx_time = Simulator::Now () + packet_tx_time;
+    if (busy_period_packet_uid == 0 || finish_tx_time >= busy_period_finish_time)
+      {
+        busy_period_packet_uid = packet_uid;
+        busy_period_finish_time = finish_tx_time;
+        NS_LOG_LOGIC ("Updating busy period info: " << busy_period_packet_uid << " " << busy_period_finish_time);
+      }
+  }
+    
+  bool
+  AlohaMacModel::HasCollided (uint64_t packet_uid)
+  {
+    NS_LOG_FUNCTION (this << packet_uid);
+    
+    bool has_collided = busy_period_collision;
+    if (has_collided)
+      {
+        NS_LOG_LOGIC ("Packet " << packet_uid << " discarded due to collision");
+      }
+    else
+      {
+        NS_LOG_LOGIC ("Packet " << packet_uid << " correctly received");
+      }
+    
+    if (busy_period_packet_uid == packet_uid)
+      {
+        busy_period_packet_uid = 0;
+        busy_period_finish_time = Simulator::Now ();
+        busy_period_collision = false;
+        NS_LOG_LOGIC ("Cleaning busy period info");
+      }
+    return has_collided;
+  }
+  
+}
+}
---
+++ model/aloha-mac-model.h
@@ -0,0 +1,51 @@
+/* -*- Mode:C++; c-file-style:"gnu"; indent-tabs-mode:nil; -*- */
+/*
+ *
+ * Copyright (c) 2021 Universidade de Vigo
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation;
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
+ * Author: Sergio Herrería Alonso <sha@det.uvigo.es>
+ *
+ */
+
+#ifndef ALOHA_MAC_MODEL_H
+#define ALOHA_MAC_MODEL_H
+
+#include "mac-model.h"
+#include "ns3/nstime.h"
+
+namespace ns3 {
+namespace icarus {
+
+  class AlohaMacModel : public MacModel
+  {
+  public:
+    static TypeId GetTypeId (void);
+    AlohaMacModel ();
+    
+    virtual MacProtocol GetMacProtocol (void) const override;
+    virtual void NewPacketRx (uint64_t packet_uid, Time packet_tx_time) override;
+    virtual bool HasCollided (uint64_t packet_uid) override;
+    
+  private:
+    uint64_t busy_period_packet_uid;
+    Time busy_period_finish_time;
+    bool busy_period_collision;
+  };
+    
+}
+}
+
+#endif
