18b0f2f22850f995c32de6bc60801f1a15eefdde
Pablo Iglesias Sanuy <pablo12oficial@gmail.com>
2021-12-09 11:48:44 +0100

    Remove unused functions of ISL Grid example and reduce timeout

--- examples/isl-grid.cc
+++ examples/isl-grid.cc
@@ -42,79 +42,6 @@ namespace ns3 {
 namespace icarus {
 
 namespace {
-constexpr double EARTH_RADIUS = 6371.009e3;
-constexpr double EARTH_SEMIMAJOR_AXIS = 6378137;
-constexpr double EARTH_GRS80_ECCENTRICITY = 0.0818191910428158;
-constexpr double EARTH_WGS84_ECCENTRICITY = 0.0818191908426215;
-constexpr double DEG2RAD = M_PI / 180.0;
-constexpr double RAD2DEG = 180.0 * M_1_PI;
-// ndnSIM version of ns-3 lacks this method. Copy it here.
-Vector
-CartesianToGeographicCoordinates (Vector pos, GeographicPositions::EarthSpheroidType sphType)
-{
-  double a; // semi-major axis of earth
-  double e; // first eccentricity of earth
-  if (sphType == GeographicPositions::SPHERE)
-    {
-      a = EARTH_RADIUS;
-      e = 0;
-    }
-  else if (sphType == GeographicPositions::GRS80)
-    {
-      a = EARTH_SEMIMAJOR_AXIS;
-      e = EARTH_GRS80_ECCENTRICITY;
-    }
-  else // if sphType == WGS84
-    {
-      a = EARTH_SEMIMAJOR_AXIS;
-      e = EARTH_WGS84_ECCENTRICITY;
-    }
-
-  Vector lla, tmp;
-  lla.y = atan2 (pos.y, pos.x); // longitude (rad), in +/- pi
-
-  double e2 = e * e;
-  // sqrt (pos.x^2 + pos.y^2)
-  double p = CalculateDistance (pos, {0, 0, pos.z});
-  lla.x = atan2 (pos.z, p * (1 - e2)); // init latitude (rad), in +/- pi
-
-  do
-    {
-      tmp = lla;
-      double N = a / sqrt (1 - e2 * sin (tmp.x) * sin (tmp.x));
-      double v = p / cos (tmp.x);
-      lla.z = v - N; // altitude
-      lla.x = atan2 (pos.z, p * (1 - e2 * N / v));
-  }
-  // 1 m difference is approx 1 / 30 arc seconds = 9.26e-6 deg
-  while (fabs (lla.x - tmp.x) > 0.00000926 * DEG2RAD);
-
-  lla.x *= RAD2DEG;
-  lla.y *= RAD2DEG;
-
-  // canonicalize (latitude) x in [-90, 90] and (longitude) y in [-180, 180)
-  if (lla.x > 90.0)
-    {
-      lla.x = 180 - lla.x;
-      lla.y += lla.y < 0 ? 180 : -180;
-    }
-  else if (lla.x < -90.0)
-    {
-      lla.x = -180 - lla.x;
-      lla.y += lla.y < 0 ? 180 : -180;
-    }
-  if (lla.y == 180.0)
-    lla.y = -180;
-
-  // make sure lat/lon in the right range to double check canonicalization
-  // and conversion routine
-  NS_ASSERT_MSG (-180.0 <= lla.y, "Conversion error: longitude too negative");
-  NS_ASSERT_MSG (180.0 > lla.y, "Conversion error: longitude too positive");
-  NS_ASSERT_MSG (-90.0 <= lla.x, "Conversion error: latitude too negative");
-  NS_ASSERT_MSG (90.0 >= lla.x, "Conversion error: latitude too positive");
-
-  return lla;
-}
 
 void
 logLinks (const Ptr<Constellation> &constellation)
@@ -156,7 +83,7 @@ main (int argc, char **argv) -> int
   icarusHelper.Install (nodes, &constellationHelper);
   islHelper.Install (nodes, &constellationHelper);
 
-  ns3::Simulator::Stop (Days (7));
+  ns3::Simulator::Stop (Seconds (2));
 
   ns3::Simulator::Schedule (Seconds (0.0), logLinks, constellationHelper.GetConstellation ());
 
